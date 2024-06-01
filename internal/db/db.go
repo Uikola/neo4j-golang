86a95f52d7014a24a6550fcfcde0f8e9ee9cc00d
ugulaev <ugulaev806@yandex.ri>
2024-06-01 07:59:25 +0000

    Add Neo4j initialization function

---
+++ internal/db/db.go
@@ -0,0 +1,20 @@
+package db
+
+import (
+	"context"
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+	"github.com/rs/zerolog/log"
+	"os"
+)
+
+func InitNeo4j(ctx context.Context) neo4j.DriverWithContext {
+	driver, err := neo4j.NewDriverWithContext(
+		os.Getenv("DB_URI"),
+		neo4j.BasicAuth(os.Getenv("DB_USER"), os.Getenv("DB_PASS"), ""))
+	err = driver.VerifyConnectivity(ctx)
+	if err != nil {
+		log.Err(err).Msg("failed to verify neo4j db connectivity")
+		os.Exit(1)
+	}
+	return driver
+}
