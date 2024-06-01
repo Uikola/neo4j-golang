84453254abf4874b50be6396d8e21c806666cc67
ugulaev <ugulaev806@yandex.ri>
2024-06-01 12:24:03 +0000

    Fix db bug

--- internal/db/db.go
+++ internal/db/db.go
@@ -11,6 +11,11 @@ func InitNeo4j(ctx context.Context) neo4j.DriverWithContext {
 	driver, err := neo4j.NewDriverWithContext(
 		os.Getenv("DB_URI"),
 		neo4j.BasicAuth(os.Getenv("DB_USER"), os.Getenv("DB_PASS"), ""))
+	if err != nil {
+		log.Error().Err(err).Msg("failed to create neo4j driver with context")
+		os.Exit(1)
+	}
+
 	err = driver.VerifyConnectivity(ctx)
 	if err != nil {
 		log.Err(err).Msg("failed to verify neo4j db connectivity")
